71685a3b5c3c49b8976a3bf55d94fc767b1771be
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-12-21 15:13:27 +0100

    another animation

--- App.js
+++ App.js
@@ -1,42 +1,32 @@
 import {AnimationDemo, AnimationElement, Padding, Size} from './Components';
-import {
-  Canvas,
-  Spring,
-  useTouchHandler,
-  useValue,
-} from '@shopify/react-native-skia';
+import {Canvas, Easing, useLoop} from '@shopify/react-native-skia';
 import {Dimensions, StyleSheet} from 'react-native';
 
 import React from 'react';
-import {runSpring} from '@shopify/react-native-skia/src/animation/Animation/functions';
+import {createTiming} from '@shopify/react-native-skia/src/animation/Animation/functions';
 
 const {width} = Dimensions.get('window');
 
-const AnimationWithTouchHandler = () => {
-  const translateX = useValue((width - Size - Padding) / 2);
-  const offsetX = useValue(0);
-  const touchHandler = useTouchHandler({
-    onStart: ({x}) => (offsetX.value = x - translateX.value),
-    onActive: ({x}) => (translateX.value = x - offsetX.value),
-    onEnd: ({velocityX}) => {
-      runSpring(
-        translateX,
-        (width - Size - Padding) / 2,
-        Spring.Wobbly({velocity: velocityX}),
-      );
-    },
-  });
-
+const InterpolationWithEasing = () => {
+  const progress = useLoop(
+    createTiming({
+      from: 10,
+      to: width - Size - Padding,
+      duration: 1000,
+      easing: Easing.inOut(Easing.cubic),
+    }),
+    {yoyo: true},
+  );
   return (
-    <AnimationDemo title={'Animation with touch handler.'}>
-      <Canvas style={styles.canvas} onTouch={touchHandler}>
-        <AnimationElement x={() => translateX.value} />
+    <AnimationDemo title={'Interpolating value using an easing.'}>
+      <Canvas style={styles.canvas}>
+        <AnimationElement x={() => progress.value} />
       </Canvas>
     </AnimationDemo>
   );
 };
 
-export default AnimationWithTouchHandler;
+export default InterpolationWithEasing;
 
 const styles = StyleSheet.create({
   canvas: {
