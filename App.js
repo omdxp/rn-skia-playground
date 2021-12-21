199602f472ae6dcee16a9942ee39054cc80c10c2
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-12-21 14:51:43 +0100

    filters

--- App.js
+++ App.js
@@ -1,108 +1,43 @@
-import {Button, StyleSheet, View} from 'react-native';
 import {
-  PaintStyle,
+  Canvas,
+  ImageShader,
+  Paint,
+  Rect,
+  Shader,
   Skia,
-  SkiaView,
-  StrokeCap,
-  useDrawCallback,
-  usePaint,
-  useTouchHandler,
+  mix,
+  useLoop,
 } from '@shopify/react-native-skia';
-import React, {useMemo, useRef} from 'react';
 
-const DrawingExample = () => {
-  const paint = usePaint(p => p.setColor(Skia.Color('#7FC8A9')));
-  const prevPointRef = useRef();
+import {Dimensions} from 'react-native';
+import React from 'react';
 
-  const pathPaint = usePaint(p => {
-    p.setColor(Skia.Color('#7F33A9'));
-    p.setStrokeWidth(5);
-    p.setStyle(PaintStyle.Stroke);
-    p.setStrokeCap(StrokeCap.Round);
-  });
+const {width, height} = Dimensions.get('window');
 
-  const paths = useMemo(() => [], []);
+const source = Skia.RuntimeEffect.Make(`
+uniform shader image;
+uniform float r;
+half4 main(float2 xy) {   
+  xy.x += sin(xy.y / r) * 4;
+  return image.eval(xy).rbga;
+}`);
 
-  const touchHandler = useTouchHandler({
-    onStart: ({x, y}) => {
-      const path = Skia.Path.Make();
-      paths.push(path);
-      path.moveTo(x, y);
-      prevPointRef.current = {x, y};
-    },
-    onActive: ({x, y}) => {
-      // Get current path object
-      const path = paths[paths.length - 1];
-
-      // Calculate and draw a smooth curve
-      const xMid = (prevPointRef.current.x + x) / 2;
-      const yMid = (prevPointRef.current.y + y) / 2;
-
-      path.quadTo(prevPointRef.current.x, prevPointRef.current.y, xMid, yMid);
-
-      prevPointRef.current = {x, y};
-    },
-  });
-
-  const onDraw = useDrawCallback(
-    (canvas, info) => {
-      // Update from pending touches
-      touchHandler(info.touches);
-
-      // Clear screen
-      canvas.drawPaint(paint);
-
-      // Draw paths
-      if (paths.length > 0) {
-        for (let i = 0; i < paths.length; i++) {
-          canvas.drawPath(paths[i], pathPaint);
-        }
-      }
-    },
-    [paint, pathPaint, paths],
-  );
-
-  const skiaViewRef = useRef(null);
-
+const Filters = () => {
+  const progress = useLoop({duration: 1500}, {yoyo: true});
   return (
-    <>
-      <SkiaView
-        ref={skiaViewRef}
-        style={styles.skiaview}
-        onDraw={onDraw}
-        debug
-      />
-      <View style={styles.buttons}>
-        <Button
-          title="Clear"
-          onPress={() => {
-            paths.length = 0;
-            skiaViewRef.current?.redraw();
-          }}
-        />
-        <Button
-          title="Undo"
-          onPress={() => {
-            paths.length = Math.max(0, paths.length - 1);
-            skiaViewRef.current?.redraw();
-          }}
-        />
-      </View>
-    </>
+    <Canvas style={{width, height}}>
+      <Paint>
+        <Shader source={source} uniforms={() => [mix(progress.value, 1, 100)]}>
+          <ImageShader
+            source={require('./oslo.jpg')}
+            fit="cover"
+            fitRect={{x: 0, y: 0, width, height}}
+          />
+        </Shader>
+      </Paint>
+      <Rect x={0} y={0} width={width} height={height} />
+    </Canvas>
   );
 };
 
-export default DrawingExample;
-
-const styles = StyleSheet.create({
-  skiaview: {
-    width: '100%',
-    flex: 1,
-    overflow: 'hidden',
-  },
-  buttons: {
-    flexDirection: 'row',
-    paddingBottom: 24,
-    paddingHorizontal: 14,
-  },
-});
+export default Filters;
